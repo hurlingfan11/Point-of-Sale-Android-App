c26123d3beda11ff3a4db2417359f9af8dd2be32
hurlingfan11 <39068491+hurlingfan11@users.noreply.github.com>
2018-10-13 16:10:05 +0100

    Basic Menu Usage

--- Point-of-Sale/app/src/main/java/com/clancy/conor/pointofsaleandroidapp/MainActivity.java
+++ Point-of-Sale/app/src/main/java/com/clancy/conor/pointofsaleandroidapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.clancy.conor.pointofsaleandroidapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -81,13 +83,20 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()){
+            case R.id.action_reset:
+                // will use the default constructor - nothing
+                mCurrentItem = new Item();
+                // make an intent to launch settings.
+                showCurrentItem();
+                break;
+            case R.id.action_settings:
+                startActivity(new Intent(Settings.ACTION_LOCALE_SETTINGS));
+                break;
+            default:
+                break;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
